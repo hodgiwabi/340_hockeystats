64aaa8e78185d0ce5d8d8a011bfd31b42d8fb31f
gs291 <sanchez.gregory886@gmail.com>
2020-02-10 20:30:38 -0800

    Fix running application

--- hockeystats/app.py
+++ hockeystats/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from jinja2 import Environment, PackageLoader, select_autoescape
 from db_connector.db_connector import connect_to_database, execute_query
 
-app = Flask(name)
+app = Flask(__name__)
 
 env = Environment(
     loader=PackageLoader('340_hockeystats', 'templates'),
