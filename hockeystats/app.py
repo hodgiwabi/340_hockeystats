aeb3bbe2ba7b298fe34ed6b28e852024c730f4ea
hodgesto <hodgesto@oregonstate.edu>
2020-02-17 19:50:01 -0800

    add cascade to foreign keys

--- hockeystats/app.py
+++ hockeystats/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("layouts/main.html",
-                           body="<i>Are you looking for /teams, /games, /players, or /penalties ?")
+                           body="<i>Are you looking for /teams, /games, /players, /penalties, or /infractions ?")
 
 
 @app.route('/teams')
