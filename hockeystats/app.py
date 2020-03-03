677749ac45607b36afc4e3020c554f015cddfb3b
gs291 <sanchez.gregory886@gmail.com>
2020-03-02 20:53:45 -0800

    project step 5 draft

--- hockeystats/app.py
+++ hockeystats/app.py
@@ -15,18 +15,28 @@ def index():
 def teams():
     print("Querying the database for Teams")
     db_connection = connect_to_database()
-
+    
     if request.method == 'GET':
         query = "SELECT team_id, team_name FROM teams;"
         result = execute_query(db_connection, query)
         return render_template("layouts/main.html",
-                               body=render_template("teams.html", rows=result))
+                            body=render_template("teams.html", rows=result))
     elif request.method == 'POST':
-        print("Adding some new team!")
-        query = "SELECT team_id, team_name FROM teams;"
-        result = execute_query(db_connection, query)
-        return render_template("layouts/main.html",
-                               body=render_template("teams.html", rows=result))
+        req = request.form["submit"]
+        if req and req == "search":
+            team_name = request.form['teamName']
+            data = [team_name]
+            query = "SELECT team_name FROM teams WHERE team_name = '%s';"
+            result = execute_query(db_connection, query, data)
+            return render_template("layouts/main.html",
+                                   body=render_template("teams.html", rows=result))
+        else:
+            teamName = request.form['teamName']
+            data = [teamName]
+            query = 'INSERT INTO teams (team_name) VALUES (%s);'
+            execute_query(db_connection, query, data)
+            return render_template("layouts/main.html", 
+                                body=render_template("posts/team_post.html", teamName=teamName))
 
 
 @app.route('/games', methods=['POST', 'GET'])
@@ -40,11 +50,17 @@ def games():
         return render_template("layouts/main.html",
                                body=render_template("games.html", rows=result))
     elif request.method == 'POST':
-        print("Adding some new game!")
-        query = "SELECT game_id, home_id, away_id, game_date, game_time FROM games;"
-        result = execute_query(db_connection, query)
+        home_id = request.form['teamAdd1']
+        away_id = request.form['teamAdd2']
+        game_date = request.form['addDate']
+        date = game_date.split('T')
+        game_date = date[0]
+        game_time = date[1]
+        query = "INSERT INTO games (home_id, away_id, game_date, game_time) VALUES (%s, %s, %s, %s);"
+        data = [home_id, away_id, game_date, game_time]
+        execute_query(db_connection, query, data)
         return render_template("layouts/main.html",
-                               body=render_template("games.html", rows=result))
+                               body=render_template("posts/game_post.html", data=data))
 
 
 @app.route('/players', methods=['POST', 'GET'])
@@ -56,13 +72,30 @@ def players():
         query = "SELECT player_id, fname, lname, number FROM players;"
         result = execute_query(db_connection, query)
         return render_template("layouts/main.html",
-                               body=render_template("players.html", rows=result))
+                            body=render_template("players.html", rows=result))
     elif request.method == 'POST':
-        print("Adding some new player!")
-        query = "SELECT player_id, fname, lname, number FROM players;"
-        result = execute_query(db_connection, query)
-        return render_template("layouts/main.html",
-                               body=render_template("players.html", rows=result))
+        print(request.form)
+        req = request.form["search"]
+        print(req)
+        if req and req == "search":
+            print("we got here")
+            number = request.form["searchPlayer"]
+            query = "SELECT player_id, fname, lname, number FROM players WHERE number = %s;"
+            data = [number]
+            result = execute_query(db_connection, query, data)
+            return render_template("layouts/main.html",
+                                body=render_template("players.html", rows=result))
+        elif req == "add":
+            fname = request.form['playerFirstName']
+            lname = request.form['playerLastName']
+            number = request.form['playerNumber']
+            team_id = request.form['playerTeam']
+
+            query = "INSERT INTO players (fname, lname, number, team_id) VALUES (%s, %s, %s, %s);"
+            data = [fname, lname, number, team_id]
+            execute_query(db_connection, query, data)
+            return render_template("layouts/main.html",
+                                body=render_template("posts/player_post.html", data=data))
 
 
 @app.route('/penalties', methods=['POST', 'GET'])
@@ -76,11 +109,12 @@ def penalties():
         return render_template("layouts/main.html",
                                body=render_template("penalties.html", rows=result))
     elif request.method == 'POST':
-        print("Adding some new penalty!")
-        query = "SELECT penalty_id, type FROM penalties;"
-        result = execute_query(db_connection, query)
+        ptype = request.form['penaltyType']
+        data = [ptype]
+        query = "INSERT INTO penalties (type) VALUES (%s);"
+        execute_query(db_connection, query, data)
         return render_template("layouts/main.html",
-                               body=render_template("penalties.html", rows=result))
+                               body=render_template("posts/penalty_post.html", data=data))
 
 
 @app.route('/infractions', methods=['POST', 'GET'])
@@ -94,8 +128,10 @@ def infractions():
         return render_template("layouts/main.html",
                                body=render_template("infractions.html", rows=result))
     elif request.method == 'POST':
-        print("Adding some new infraction!")
-        query = "SELECT infraction_id, player_id, penalty_id FROM infractions;"
-        result = execute_query(db_connection, query)
+        player_id = request.form['infractionPlayerID']
+        penalty_id = request.form['infractionPenaltyIDs']
+        data = [player_id, penalty_id]
+        query = "INSERT INTO infractions (player_id, penalty_id) VALUES (%s, %s);"
+        execute_query(db_connection, query, data)
         return render_template("layouts/main.html",
-                               body=render_template("infractions.html", rows=result))
+                               body=render_template("posts/infraction_post.html", data=data))
